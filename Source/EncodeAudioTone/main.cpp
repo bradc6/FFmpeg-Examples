7f0a1c630ff57159d224fa6a2d752b45d67f4f26
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2013-07-30 06:18:19 -0700

    *Removed Boost *Updated the FindFFMPEG.cmake for OSX

--- Source/EncodeAudioTone/main.cpp
+++ Source/EncodeAudioTone/main.cpp
@@ -1,7 +1,33 @@
+#include "main.hpp"
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello World\n";
+	std::cout << "Encode Tone into MP2 format\n"
+              << "The output file will be encodeAudioOutput.mp2";
+    
+    //The desired codec that we wish to use for encoding
+    //for simpilicity sake we will use MP2
+    AVCodec *mp2Codec;
+    
+    //The codec context sets difference aspects of the audio
+    //such as the desired bitrate, number of audio channels,
+    //sample rate and other audio aspects
+    AVCodecContext *mp2EncoderProperties;
+    
+    //Have FFMPEG see if that specific codec is compiled into the
+    //library.
+     mp2Codec = avcodec_find_encoder(AV_CODEC_ID_MP2);
+    //Check the pointer to confirm that the encoder was found
+    //beware that a codec may have a DECODER, but no encoder
+    //such as SMACKER (as of 7-30-2013)
+    if(!mp2Codec)
+    {
+        std::cout << "Sorry, looks like FFMPEG does not have that codec :(\n";
+        exit(-1);
+    }
+    
+    
+    
 	return 0;
 }
---
+++ Source/EncodeAudioTone/main.hpp
@@ -0,0 +1,12 @@
+#ifndef EncodeAudio_Header
+#define EncodeAudio_Header
+
+#include <libavutil/opt.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/common.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/samplefmt.h>
+
+#endif
